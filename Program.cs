7fbfc22bf96201d58c48a4541105d08686fd5aa0
Anderson Cravo <drinhodias@hotmail.com>
2022-04-22 22:08:46 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TabuadaUsandoFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número da tabuada:  ");
+
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = N * i;
+                Console.WriteLine(N + "x" + i + "=" + resultado);
+            }
+        }
+    }
+}
